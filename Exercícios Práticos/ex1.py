e67a981e93bd228956f4be6389823f4a9af0d5bd
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2025-06-18 12:30:46 -0300

    estudos Python

---
+++ Exercícios Práticos/ex1.py
@@ -0,0 +1,14 @@
+def main():
+    print("\n=====Manipulando Variáveis=====\n")
+
+    nome = "Marcelo"
+    idade = 19
+    altura = 1.81
+
+    print(nome)
+    print(idade)
+    print(altura)
+
+    print(f"\nMeu nome é {nome}, tenho {idade} anos e {altura} de altura\n")
+
+main()
---
+++ Exercícios Práticos/ex3.py
@@ -0,0 +1,9 @@
+def main():
+    numero = int(input("Digite o número: "))
+
+    if  numero % 2 == 0:
+        print(f"O número {numero} é par")
+    else:
+        print(f"O número {numero} é impar")
+
+main()
