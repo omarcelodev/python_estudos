f77898a434275dca235eaec70029d8a540eab1fe
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2025-06-17 23:57:29 -0300

    python estudos

---
+++ ex2.py
@@ -0,0 +1,26 @@
+#Váriaveis em Python:
+nome = "Marcelo" #string
+idade = 19 #int
+altura = 1.81 #float
+
+print(nome)
+print(idade)
+print(altura)
+
+print("=============")
+
+a, b, c = 5, 3.54, "Hello!"
+
+print(a)
+print(b)
+print(c)
+
+print("==============")
+
+meuNome = "Marcelo"
+minhaIdade = 19
+meuEndereco = "Rua das Esmeraldas"
+
+print(meuNome)
+print(minhaIdade)
+print(meuEndereco)
---
+++ ex3.py
@@ -0,0 +1,11 @@
+#Statecally Typed não se aplica em python
+
+v = 100
+print(v)
+
+v = "Agora sou uma string"
+print(v)
+
+print(33)
+
+type(33)
