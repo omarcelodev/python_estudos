d44f72b8c22764e7f2bd59901aa7f7fd219abc4d
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2025-10-08 18:58:07 -0300

    Simplify and improve venda() flow and prompts
    Refactored the venda() function to streamline user prompts, improve input validation, and simplify the logic for handling product registration and stock checks. Replaced numeric options with 's/n' inputs and provided clearer feedback to the user.

--- Módulo 1 - Fundamentos/Aula 5 - Estrutura de Dados/desafio03.py
+++ Módulo 1 - Fundamentos/Aula 5 - Estrutura de Dados/desafio03.py
@@ -7,49 +7,29 @@ produtos = {
 def venda():
     print("===CADASTRO DE VENDAS===\n")
     while True:
-        nome = input("Digite o nome do produto vendido: ")
+        nome = input("Produto Vendido: ")
 
-        if nome in produtos:
-            quantidade = int(input("Digite a quantidade vendida: "))
-
-            if produtos[nome] == 0 or quantidade > produtos[nome]:
-                while True:
-                    print("Não há Produtos no estoque!")
-                    opcao = int(input("Deseja cadastrar uma reposição?(1)Sim (0)Não: "))
-
-                    if opcao == 1:
-                        print("Função reposição") #Adcionar depois
-                        break
-                    elif opcao == 0:
-                        break
-                    else:
-                        print("Opção Inválida!")
-
-            else:
-                produtos[nome] -= quantidade
-                print("produtos:", produtos)
-
-        else:
-            while True:
-                print("Este produto não está cadastrado")
-                opcao = int(input("Deseja cadastra-lo? (1)Sim (0)Não: "))
-
-                if opcao == 1:
-                    print("Função Cadastro") #Adicionar depois
-                    break
-                elif opcao == 0:
-                    break
-                else:
-                    print("Opção Inválida")
-        
-        opcao = int(input("Deseja cadastrar uma nova venda? (1)Sim (0)Não: "))
-
-        if opcao == 1:
+        if nome not in produtos:
+            if input("Produto não cadastrado. Deseja adicionar? (s/n): ").lower() == 's':
+                print("Função para cadastrar produto") #adicionar depois
             continue
-        elif opcao == 0:
-            break
-        else:
-            print("Opção Inválida")
+
+        quantidade = int(input("Quantidade vendida: "))
+        estoque_atual = produtos[nome]
+
+        if quantidade > estoque_atual:
+            print(f"Estoque Insuficiente! ({estoque_atual} disponiveis).")
+
+            if input("Deseja repor o estoque? (s/n): ").lower() == 's':
+                print("Função para Repor estoque") #Adicionar depois
+            continue
+
+        produtos[nome] -= quantidade
+            
+        print(f"Venda realizada! Estoque atual de {nome}: {produtos[nome]}")
+
+        if input("Deseja realizar uma nova venda? (s/n): ").lower() == 'n':
+                break
 
 def menu():
     while True:
