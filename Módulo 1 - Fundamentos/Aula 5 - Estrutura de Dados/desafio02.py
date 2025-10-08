780bf325b6d5a0239d1d3cd47ce54a563f4e9438
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2025-10-08 10:59:40 -0300

    Refatora exibição e entrada de números no desafio02.py
    Melhora a mensagem de entrada para o usuário, ajusta a exibição dos números digitados e padroniza as mensagens de saída para maior clareza. Pequenas correções de indentação e nomes de variáveis também foram realizadas.

--- Módulo 1 - Fundamentos/Aula 5 - Estrutura de Dados/desafio02.py
+++ Módulo 1 - Fundamentos/Aula 5 - Estrutura de Dados/desafio02.py
@@ -2,20 +2,21 @@ def numeros():
     lista_numeros = []
 
     for i in range(10):
-            numero = int(input(f"Digite {i + 1}° número: "))
-            lista_numeros.append(numero)
+        numeros = int(input(f"Digite o {i + 1}° numero:"))
+        lista_numeros.append(numeros)
     
-    print(lista_numeros)
+    print("Números digitados:", lista_numeros)
 
     total = soma(lista_numeros)
 
-    print("A soma dos números: ", total)
-    print("O maior número: ", max(lista_numeros))
-    print("O menor número: ", min(lista_numeros))
-    print("A média: ", total / 10)
+    print("Maior Número:", max(lista_numeros))
+    print("Menor Número:", min(lista_numeros))
+    print("Média:", total / 10)
+
 
 def soma(lista_numeros):
     soma_total = 0
+
     for i in lista_numeros:
         soma_total += i
     
