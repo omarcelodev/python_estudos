bd9e18ea9c6e1c815a3e85abf4d05f90800d176c
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2025-10-09 19:52:49 -0300

    Add reposicao function for product restocking
    Introduces the reposicao() function to handle product restocking, including prompts for product selection and quantity. Updates the menu to call reposicao() when the appropriate option is selected. Also improves formatting in venda().

--- Módulo 1 - Fundamentos/Aula 5 - Estrutura de Dados/desafio03.py
+++ Módulo 1 - Fundamentos/Aula 5 - Estrutura de Dados/desafio03.py
@@ -5,7 +5,7 @@ produtos = {
 }
 
 def venda():
-    print("===CADASTRO DE VENDAS===\n")
+    print("\n===CADASTRO DE VENDAS===\n")
     while True:
         nome = input("Produto Vendido: ")
 
@@ -31,6 +31,24 @@ def venda():
         if input("Deseja realizar uma nova venda? (s/n): ").lower() == 'n':
                 break
 
+def reposicao():
+    print("\n===CADASTRO DE REPOSIÇÃO===\n")
+
+    nome = input("Informe o produto que deseja repor: ")
+
+    if nome not in produtos:
+        if input("Produto não cadastrado. Deseja adicionar? (s/n): ").lower() == 's':
+            print("Função para cadastrar produto") # adicionar depois
+    
+    quantidade = int(input("Informe a quantidade que deseja repor: "))
+    estoque_atual = produtos[nome]
+
+    if estoque_atual == 1000:
+        print(f"Estoque máximo atingindo! ({estoque_atual}).")
+
+
+    produtos[nome] += quantidade
+
 def menu():
     while True:
         print("===CONTROLE DE ESTOQUE===\n")
@@ -38,13 +56,14 @@ def menu():
         print("(1)Venda de Produto")
         print("(2)Reposição de Produto")
         print("(3)Listamento de Produtos")
+        print("(4)Cadastrar item")
         print("(0)Sair")
         opcao = int(input("Sua opção: "))
 
         if opcao == 1:
             venda()
         elif opcao == 2:
-            print("Reposição")
+            reposicao()
         elif opcao == 3:
             print("Listamento")
         elif opcao == 0:
