d9b02446a0260a9f8497e7af973140d168b9491f
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2025-10-11 15:14:23 -0300

    Add string manipulation exercise script
    Implements a script that prompts the user for a word, checks if it starts with 'a', ends with 'o', counts occurrences of 'e', and allows repeated input until the user chooses to exit.

---
+++ Módulo 1 - Fundamentos/Aula 6 - Manipulação de Strings/exercicio03.py
@@ -0,0 +1,22 @@
+def palavra():
+    while True:
+        palavra = input("Digite uma palavra:")
+        palavra_formatada = palavra.lower()
+
+        if palavra_formatada.startswith("a"):
+            print("A palavra começa com A!")
+        else:
+            print("A palavra não começa com A!")
+
+        if palavra_formatada.endswith("o"):
+            print("A palavra termina com O!")
+        else:
+            print("A palavra não termina com O!")
+    
+        print(f"A letra 'E' Apareceu: {palavra_formatada.count("e")} vezes")
+
+        if input("Deseja escrever outra palavra? (s/n): ") == 'n':
+            break
+
+palavra()
+
