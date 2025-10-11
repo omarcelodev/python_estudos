9eeffd802fe40c905aeb9e8218f08794b9418348
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2025-10-11 12:14:23 -0300

    Move programa03.py and add string content test script
    Moved programa03.py to the Aula 6 - Manipulação de Strings directory and added programa04.py, which tests string content using various string methods and prints the results.

---
+++ Módulo 1 - Fundamentos/Aula 6 - Manipulação de Strings/programa04.py
@@ -0,0 +1,26 @@
+#Testando contéudo da string
+#Retorna true ou false dependendo do contéudo
+
+def true_or_false(teste):
+    if teste == True:
+        print("True")
+    else:
+        print("False")
+
+texto = input("Frase: ")
+
+teste = texto.isaplha() #True se so tem letras
+true_or_false(teste)
+
+teste = texto.isdigit() #True se so tem numeros
+true_or_false(teste)
+
+teste = texto.isalnum() #True se tem letras e/ou numeros
+true_or_false(teste)
+
+teste = texto.startswith("Py") #True se começa com "Py"
+true_or_false(teste)
+
+teste = texto.endswith("on") #True se termina com "on"
+true_or_false(teste)
+
