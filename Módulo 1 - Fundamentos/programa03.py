d500fdcf717bba642ea760dad7570436543b1135
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2025-10-11 12:03:23 -0300

    Add examples of string methods and functions
    Created programa03.py with demonstrations of common string methods in Python, including upper(), lower(), title(), strip(), replace(), split(), join(), find(), and count().

---
+++ Módulo 1 - Fundamentos/programa03.py
@@ -0,0 +1,19 @@
+#Principais Metodos e Funções de strings
+
+print("python".upper())
+
+print("PYTHON".lower())
+
+print("python é bom".title())
+
+print("   oi   ".strip())
+
+print("banana".replace("na", "ne"))
+
+print("um dois".split())
+
+print(''.join(['um', 'dois']))
+
+print("banana".find("na"))
+
+print("banana".count("a"))
