ff0c9309cd49648979a9cdbec51f7d94d7501849
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2025-10-11 11:52:16 -0300

    Add string slicing and indexing examples in Python
    Introduces a new script demonstrating how to access characters and slice strings in Python using indexing and slicing techniques.

---
+++ Módulo 1 - Fundamentos/Aula 6 - Manipulação de Strings/programa02.py
@@ -0,0 +1,13 @@
+#Acessando caracteres e fatiando
+
+#Cada caractere tem um índice começando com 0 [0] ; [1] ; [2] ....
+
+#Para fatiar pedaços se usa [:]
+
+texto = "Python"
+
+print(texto[0])
+print(texto[-1])
+print(texto[0:3])
+print(texto[2:])
+print(texto[::-1])
