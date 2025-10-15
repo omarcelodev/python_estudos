289b0ecfc9d9d35f99fed495d53290c6675525f1
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2025-10-15 13:42:38 -0300

    Add script to collect and validate user info
    Introduces a Python script that prompts the user for their name, age, and city, validates the age and city against predefined criteria, and displays a welcome message. The script includes input validation for age (1-100) and checks if the city is a Brazilian state capital.

---
+++ Módulo 1 - Fundamentos/Aula 7 - Entradas e Saídas/desafio03.py
@@ -0,0 +1,67 @@
+capitais_brasileiras = [
+    "riobranco",      # Acre
+    "maceio",        # Alagoas
+    "macapa",        # Amapa
+    "manaus",        # Amazonas
+    "salvador",         # Bahia
+    "fortaleza",     # Ceara
+    "brasilia",      # Distrito Federal
+    "vitoria",       # Espirito Santo
+    "goiania",       # Goias
+    "saoluis",      # Maranhao
+    "cuiaba",        # Mato Grosso
+    "campogrande",        # Mato Grosso do Sul
+    "belohorizonte",  # Minas Gerais
+    "belem",         # Para
+    "joaopessoa",   # Paraiba
+    "curitiba",      # Parana
+    "recife",        # Pernambuco
+    "teresina",      # Piaui
+    "riodejaneiro",# Rio de Janeiro
+    "natal",         # Rio Grande do Norte
+    "portoalegre",  # Rio Grande do Sul
+    "portovelho",   # Rondonia
+    "boavista",     # Roraima
+    "florianopolis", # Santa Catarina
+    "saopaulo",     # Sao Paulo
+    "aracaju",       # Sergipe
+    "palmas"         # Tocantins
+]
+#Se idade for inválida retorna False
+def verificar_idade(idade):
+    if idade < 1 or idade > 100:
+        print("Idade Inválida! Tente novamente.")
+        return False
+    else:
+        return True
+
+#Se a cidade for inálida retorna True
+def verificar_cidade(cidade_formatada):
+    if cidade_formatada not in capitais_brasileiras:
+        print("Essa cidade não está disponível para escolha!")
+        return True
+    else:
+        return False
+    
+
+def mensagem():
+    while True:
+        nome = input("Digite sue nome: ").strip()
+
+        while verificar_idade(idade):
+            idade = int(input("Digite sua idade: "))
+    
+        while True:
+            cidade = input("Difite o nome da sua cidade: ")
+            cidade_formatada = cidade.lower()
+            cidade_formatada = cidade_formatada.replace(" ", "")
+
+            if verificar_cidade(cidade_formatada) == False:
+                break
+
+        print(f"Olá, {nome}! Você tem {idade} anos e mora em {cidade}. Seja bem-vindo!")
+
+        if input("Deseja encerrar o programa?(s/n): ").lower() == 's':
+            break
+
+mensagem()
