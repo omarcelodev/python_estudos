e6b6150114a6ccf9ad866608f251c4eb0b643551
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2025-10-17 09:54:03 -0300

    Add examples using math module in Python
    Created programa02.py to demonstrate usage of math module functions such as sqrt, pow, pi, ceil, and floor. Added an empty programa03.py as a placeholder for future content.

---
+++ Módulo 1 - Fundamentos/Aula 8 - Módulos Padrão/programa02.py
@@ -0,0 +1,9 @@
+#Módulo math - matemática pura e dura
+
+import math
+
+print(math.sqrt(16)) # Raiz Quadrada
+print(math.pow(2, 3)) # Potência
+print(math.pi) # Constante PI
+print(math.ceil(3.2)) # Arredonda pra cima
+print(math.floor(3.8)) # Arredonda pra baixo
