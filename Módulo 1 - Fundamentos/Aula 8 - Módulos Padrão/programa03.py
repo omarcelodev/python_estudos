6da46263d80146b67a219c3c1d94fde607b5d679
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2025-10-17 09:56:18 -0300

    Add examples using Python random module
    Introduces usage of the random module with demonstrations of randint, choice, and random functions for generating random numbers and selecting random items from a list.

---
+++ Módulo 1 - Fundamentos/Aula 8 - Módulos Padrão/programa03.py
@@ -0,0 +1,7 @@
+# Módulo Random - Aleatoriedade Total
+
+import random
+
+print(random.randint(1, 10)) #Número inteiro entre 1 e 10
+print(random.choice(['Maçã', 'Pera', 'Uva'])) #Escolhe um item da lista
+print(random.random()) #Número float entre 0 e 1
